baa28424f0aa57ef25aa04c17b4dc018fb2d1301
リル姉のMac <95098112+chocorone@users.noreply.github.com>
2022-11-19 01:15:06 +0900

    [Add] NovelPlayer、入力方法の追加

---
+++ Assets/NovelEditor/Sripts/Controller/NovelPlayer.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+using static NovelData;
+
+[RequireComponent(typeof(NovelInputProvider))]
+public class NovelPlayer : MonoBehaviour
+{
+    [SerializeField] NovelData noveldata;
+
+    [SerializeField] bool playOnAwake;
+    [SerializeField] bool hideAfterPlay;
+    [SerializeField] HowInput inputSystem;
+    [SerializeField] KeyCode[] nextButton;
+    [SerializeField] KeyCode[] skipButton;
+    [SerializeField] KeyCode[] hideOrDisplayButton;
+    [SerializeField] KeyCode[] stopOrStartButton;
+
+
+    NovelInputProvider inputProvider;
+
+    void Awake()
+    {
+        switch (inputSystem)
+        {
+            case HowInput.Auto:
+                inputProvider = new DefaultInputProvider();
+                break;
+            case HowInput.UserSetting:
+                inputProvider = new CustomInputProvider(nextButton, skipButton, hideOrDisplayButton, stopOrStartButton);
+                Debug.Log("aaa");
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if (inputProvider.GetNext())
+        {
+            Debug.Log("GetNext");
+        }
+        if (inputProvider.GetSkip())
+        {
+            Debug.Log("GetSkip");
+        }
+    }
+}
