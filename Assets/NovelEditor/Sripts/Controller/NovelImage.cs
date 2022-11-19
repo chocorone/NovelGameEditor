25c99bc7db448155fb7285445f4b451961b07e93
リル姉 <95098112+chocorone@users.noreply.github.com>
2022-11-19 22:28:18 +0900

    [Add] フォントの追加

--- Assets/NovelEditor/Sripts/Controller/NovelImage.cs
+++ Assets/NovelEditor/Sripts/Controller/NovelImage.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class NovelImage : MonoBehaviour
 {
-    public void QuickChange(Sprite next)
+    protected Image _image;
+    private Color _defaultColor;
+
+    public void Change(Sprite next)
     {
 
     }
+
+    void Awake(){
+        _image = GetComponent<Image>();
+        _defaultColor = _image.color;
+        Debug.Log("parent");
+    }
+
+    // async UniTask<bool> FadeOut(Color color,float fadeTime){
+    //     float alpha = 0;
+
+    //     while (alpha<1)
+    //     {
+    //         _image.color.a = alpha;
+    //         _image.color = Color.Lerp(_defaultColor,color,alpha);
+    //         await UniTask.Delay(fadeTime*0.01);
+    //         alpha += 0.01f;
+    //     }
+
+    //     _image.color.a = 1;
+    //     return true;
+    // }
+
+    void FadeIn(){
+
+    }
+
 }
