f56055d1072ec95760be8d034c6ebfbb77fb4ef0
リル姉のMac <95098112+chocorone@users.noreply.github.com>
2022-11-20 00:59:15 +0900

    [Update] テキストを1文字ずつ再生するように

--- Assets/NovelEditor/Sripts/Controller/DialogueText.cs
+++ Assets/NovelEditor/Sripts/Controller/DialogueText.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using static NovelData.ParagraphData;
+using Cysharp.Threading.Tasks;
+using System;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class DialogueText : MonoBehaviour
 {
+    TextMeshProUGUI tmpro;
+    int textSpeed = 6;
+
+    void Awake()
+    {
+        tmpro = GetComponent<TextMeshProUGUI>();
+    }
     public void textUpdate(Dialogue data)
     {
-        Debug.Log(data.text);
+        //再生が終わったら通知したい
+        PlayText(data.text);
     }
 
-    void PlayText(string text)
+    private async UniTask<bool> PlayText(string text)
     {
+        tmpro.text = "";
 
+        int wordcnt = 0;
+        while (wordcnt < text.Length)
+        {
+            await UniTask.Delay(textSpeed * 10);
+
+            tmpro.text += text[wordcnt];
+
+            // if (IsStop)
+            // {
+            //     await UniTask.WaitUntil(() => !IsStop);
+            // }
+            wordcnt++;
+        }
+
+        return true;
     }
 }
