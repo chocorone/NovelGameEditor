18d568c19ab4495ac01f2579ebfad0c023227b3a
リル姉のMac <95098112+chocorone@users.noreply.github.com>
2022-11-22 23:01:30 +0900

    [Add] EffectManager、シェーダーを追加

--- Assets/NovelEditor/Runtime/Controller/EffectManager.cs
+++ Assets/NovelEditor/Runtime/Controller/EffectManager.cs
@@ -1,16 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace NovelEditorPlugin
 {
     public class EffectManager
     {
-        Shader shader;
+        static EffectManager instance;
+        public static EffectManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EffectManager();
+                }
+                return instance;
+            }
+        }
+
+        Shader None;
+        Shader Noise;
+        Shader Mosaic;
+        Shader GrayScale;
+        Shader Sepia;
+        Shader Jaggy;
+        Shader Holo;
+        Shader ChromaticAberration;
+        Shader Blur;
 
         public EffectManager()
         {
-            shader = Resources.Load<Shader>("EditorBackGround");
+            None = Resources.Load<Shader>("DefaultEffect");
+            Noise = Resources.Load<Shader>("NoiseEffect");
+            Mosaic = Resources.Load<Shader>("MosaicEffect");
+            GrayScale = Resources.Load<Shader>("GrayScaleEffect");
+            Sepia = Resources.Load<Shader>("SepiaEffect");
+            Jaggy = Resources.Load<Shader>("JaggyEffect");
+            Holo = Resources.Load<Shader>("HoloEffect");
+            ChromaticAberration = Resources.Load<Shader>("ChromaticEffect");
+            Blur = Resources.Load<Shader>("BlurEffect");
+        }
+
+        public void SetEffect(Image image, Effect effect)
+        {
+            switch (effect)
+            {
+                case Effect.None:
+                    break;
+                case Effect.Noise:
+                    break;
+                case Effect.Mosaic:
+                    break;
+                case Effect.GrayScale:
+                    break;
+                case Effect.Sepia:
+                    break;
+                case Effect.Jaggy:
+                    break;
+                case Effect.Holo:
+                    break;
+                case Effect.ChromaticAberration:
+                    break;
+                case Effect.Blur:
+                    break;
+            }
+
+            if (effect != Effect.None && effect != Effect.UnChange)
+            {
+
+            }
         }
 
     }
---
+++ Assets/NovelEditor/Runtime/Resources/GrayScaleEffect.shader
@@ -0,0 +1,54 @@
+Shader "NovelEditor/GrayScaleEffect"
+{
+	Properties
+	{
+		_MainTex("Sprite Texture", 2D) = "white" {}
+        _Strength("Strength", float) = 1
+	}
+    SubShader
+    {
+        Tags { "QUEUE"="Transparent" "RenderType"="Transparent" }
+        Cull Off ZWrite Off ZTest Always Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+            float _Strength;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+                col.rgb = dot(col.rgb, float3(0.3, 0.59, 0.11)*_Strength*0.1);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
+
