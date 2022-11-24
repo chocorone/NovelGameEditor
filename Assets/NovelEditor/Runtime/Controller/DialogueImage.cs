388c017252b99c32d079ac8ed48450ffe8c3f5dc
リル姉のMac <95098112+chocorone@users.noreply.github.com>
2022-11-24 14:23:35 +0900

    [Fix] UI用のCanvasGroupを分ける

--- Assets/NovelEditor/Runtime/Controller/DialogueImage.cs
+++ Assets/NovelEditor/Runtime/Controller/DialogueImage.cs
@@ -7,63 +7,11 @@ using System;
 
 namespace NovelEditorPlugin
 {
-    [RequireComponent(typeof(CanvasGroup))]
     public class DialogueImage : NovelImage
     {
-        private CanvasGroup dialogueCanvas;
-        // Start is called before the first frame update
         void Awake()
         {
             Init();
-            dialogueCanvas = GetComponent<CanvasGroup>();
-        }
-
-        public async UniTask<bool> DissolveIn(float time, CancellationToken token)
-        {
-            float alpha = 0;
-
-            float alphaSpeed = 0.01f;
-            if (time < 0.5)
-            {
-                alphaSpeed = 0.1f;
-            }
-            try
-            {
-                while (alpha < 1)
-                {
-                    dialogueCanvas.alpha = alpha;
-                    await UniTask.Delay(TimeSpan.FromSeconds(time * alphaSpeed), cancellationToken: token);
-                    alpha += alphaSpeed;
-                }
-            }
-            catch (OperationCanceledException)
-            { }
-
-            return true;
-        }
-
-        public async UniTask<bool> DissolveOut(float time, CancellationToken token)
-        {
-            float alpha = 1;
-
-            float alphaSpeed = 0.01f;
-            if (time < 0.5)
-            {
-                alphaSpeed = 0.1f;
-            }
-            try
-            {
-                while (alpha > 0)
-                {
-                    dialogueCanvas.alpha = alpha;
-                    await UniTask.Delay(TimeSpan.FromSeconds(time * alphaSpeed), cancellationToken: token);
-                    alpha -= alphaSpeed;
-                }
-            }
-            catch (OperationCanceledException)
-            { }
-
-            return false;
         }
     }
 }
