763b9231b56631d9318626c99feebfe28d97859b
リル姉 <95098112+chocorone@users.noreply.github.com>
2022-11-27 00:11:42 +0900

    [Fix] Skip機能の修正

--- Assets/NovelEditor/Runtime/Controller/DataLoader.cs
+++ Assets/NovelEditor/Runtime/Controller/DataLoader.cs
@@ -28,6 +28,7 @@ namespace NovelEditor
 
         internal NovelData.ParagraphData.Dialogue LoadDialogue(NovelSaveData savedData)
         {
+            progress = 0;
             NovelData.ParagraphData.Dialogue first = savedData.novelData.paragraphList[savedData.passedParagraphId[0]].dialogueList[0];
             NovelData.ParagraphData.Dialogue data = JsonUtility.FromJson<NovelData.ParagraphData.Dialogue>(JsonUtility.ToJson(first));
 
@@ -38,48 +39,7 @@ namespace NovelEditor
                 {
                     NovelData.ParagraphData.Dialogue nowDialogue = nowParagraph.dialogueList[j];
 
-                    data.back = nowDialogue.howBack != BackChangeStyle.UnChange ? nowDialogue.back : data.back;
-
-                    data.backEffect = nowDialogue.backEffect != Effect.UnChange ? nowDialogue.backEffect : data.backEffect;
-                    data.backEffectStrength = nowDialogue.backEffectStrength;
-
-                    data.DialogueEffect = nowDialogue.DialogueEffect != Effect.UnChange ? nowDialogue.DialogueEffect : data.DialogueEffect;
-                    data.DialogueEffectStrength = nowDialogue.DialogueEffectStrength;
-
-                    if (nowDialogue.changeFont)
-                    {
-                        data.font = nowDialogue.font;
-                        data.fontColor = nowDialogue.fontColor;
-                        data.fontSize = nowDialogue.fontSize;
-                    }
-
-                    if (nowDialogue.changeNameFont)
-                    {
-                        data.nameFont = nowDialogue.nameFont;
-                        data.nameColor = nowDialogue.nameColor;
-                    }
-
-                    data.SE = nowDialogue.SE;
-                    data.SEStyle = nowDialogue.SEStyle != SoundStyle.UnChange ? nowDialogue.SEStyle : data.SEStyle;
-                    data.SELoop = nowDialogue.SELoop;
-                    data.SECount = nowDialogue.SECount;
-                    data.SEFadeTime = nowDialogue.SEFadeTime;
-                    data.SEEndFadeTime = nowDialogue.SEEndFadeTime;
-
-                    data.BGM = nowDialogue.BGM;
-                    data.BGMStyle = nowDialogue.BGMStyle != SoundStyle.UnChange ? nowDialogue.BGMStyle : data.BGMStyle;
-                    data.BGMLoop = nowDialogue.BGMLoop;
-                    data.BGMCount = nowDialogue.BGMCount;
-                    data.BGMFadeTime = nowDialogue.BGMFadeTime;
-                    data.BGMEndFadeTime = nowDialogue.BGMEndFadeTime;
-
-                    for (int charaIndex = 0; charaIndex < data.charas.Length; charaIndex++)
-                    {
-                        data.charas[i] = data.howCharas[i] != CharaChangeStyle.UnChange ? nowDialogue.charas[i] : data.charas[i];
-                        data.charaEffects[i] = data.charaEffects[i] != Effect.UnChange ? nowDialogue.charaEffects[i] : data.charaEffects[i];
-                        data.charaEffectStrength[i] = data.charaEffectStrength[i];
-                    }
-
+                    SaveNext(data, nowDialogue);
 
                     if (j == savedData.dialogueIndex && i == savedData.paragraphIndex)
                     {
@@ -101,7 +61,9 @@ namespace NovelEditor
                             progress / 100);
 #endif
                 }
+#if UNITY_EDITOR
                 EditorUtility.ClearProgressBar();
+#endif
             }
 
             return data;
@@ -111,6 +73,138 @@ namespace NovelEditor
         {
             NovelSaveData savedData = new(novelData, paragraphIndex, dialogueIndex, passedParagraphId);
             return savedData;
+        }
+
+        internal SkipedData Skip(NovelData novelData, NovelData.ParagraphData nowParagraphData, int dialogueIndex, List<int> passedParagraphID, List<string> paragraphName)
+        {
+            NovelData.ParagraphData.Dialogue first = nowParagraphData.dialogueList[dialogueIndex];
+            NovelData.ParagraphData.Dialogue data = JsonUtility.FromJson<NovelData.ParagraphData.Dialogue>(JsonUtility.ToJson(first));
+            SkipedData skipData = new SkipedData();
+
+            while (true)
+            {
+
+                if (nowParagraphData.next == Next.End || (nowParagraphData.next == Next.Continue && nowParagraphData.nextParagraphIndex == -1))
+                {
+                    skipData.next = Next.End;
+                    return skipData;
+                }
+
+                foreach (var dialogue in nowParagraphData.dialogueList)
+                {
+                    SaveNext(data, dialogue);
+                }
+
+                if (nowParagraphData.next == Next.Choice)
+                {
+                    skipData.dialogue = data;
+                    skipData.next = Next.Choice;
+                    skipData.ParagraphIndex = nowParagraphData.index;
+                    return skipData;
+                }
+                else
+                {
+                    //選択肢のノードが続きになければ終わる
+                    if (nowParagraphData.next == Next.End || nowParagraphData.nextParagraphIndex == -1)
+                    {
+                        skipData.next = Next.End;
+                        return skipData;
+                    }
+                    nowParagraphData = novelData.paragraphList[nowParagraphData.nextParagraphIndex];
+                    paragraphName.Add(nowParagraphData.name);
+                    passedParagraphID.Add(nowParagraphData.index);
+                }
+            }
+        }
+
+
+        internal NovelData.ParagraphData.Dialogue SkipNextNode(NovelData novelData, NovelData.ParagraphData nowParagraphData, int dialogueIndex)
+        {
+            progress = 0;
+            NovelData.ParagraphData.Dialogue first = nowParagraphData.dialogueList[dialogueIndex];
+            NovelData.ParagraphData.Dialogue data = JsonUtility.FromJson<NovelData.ParagraphData.Dialogue>(JsonUtility.ToJson(first));
+
+            if (nowParagraphData.next == Next.End || (nowParagraphData.next == Next.Continue && nowParagraphData.nextParagraphIndex == -1))
+            {
+                return null;
+            }
+
+            for (int i = dialogueIndex + 1; i < nowParagraphData.dialogueList.Count; i++)
+            {
+                SaveNext(data, nowParagraphData.dialogueList[i]);
+                progress += 100 / (nowParagraphData.dialogueList.Count - dialogueIndex);
+
+#if UNITY_EDITOR
+                EditorUtility.DisplayProgressBar(
+                        "NovelEditor",
+                        "次のノードへスキップしています",
+                        progress / 100);
+#endif
+            }
+
+            if (nowParagraphData.next == Next.Continue)
+            {
+                SaveNext(data, novelData.paragraphList[nowParagraphData.nextParagraphIndex].dialogueList[0]);
+            }
+
+#if UNITY_EDITOR
+            EditorUtility.ClearProgressBar();
+#endif
+
+            return data;
+        }
+
+
+        void SaveNext(NovelData.ParagraphData.Dialogue data, NovelData.ParagraphData.Dialogue nowDialogue)
+        {
+            data.Name = nowDialogue.Name;
+            data.text = nowDialogue.text;
+            data.howBack = BackChangeStyle.FadeAll;
+            data.backFadeColor = Color.black;
+            data.backFadeSpeed = 1.0f;
+
+            data.back = nowDialogue.howBack != BackChangeStyle.UnChange ? nowDialogue.back : data.back;
+
+            data.backEffect = nowDialogue.backEffect != Effect.UnChange ? nowDialogue.backEffect : data.backEffect;
+            data.backEffectStrength = nowDialogue.backEffectStrength;
+
+            data.DialogueEffect = nowDialogue.DialogueEffect != Effect.UnChange ? nowDialogue.DialogueEffect : data.DialogueEffect;
+            data.DialogueEffectStrength = nowDialogue.DialogueEffectStrength;
+
+            if (nowDialogue.changeFont)
+            {
+                data.font = nowDialogue.font;
+                data.fontColor = nowDialogue.fontColor;
+                data.fontSize = nowDialogue.fontSize;
+            }
+
+            if (nowDialogue.changeNameFont)
+            {
+                data.nameFont = nowDialogue.nameFont;
+                data.nameColor = nowDialogue.nameColor;
+            }
+
+            data.SE = nowDialogue.SE;
+            data.SEStyle = nowDialogue.SEStyle != SoundStyle.UnChange ? nowDialogue.SEStyle : data.SEStyle;
+            data.SELoop = nowDialogue.SELoop;
+            data.SECount = nowDialogue.SECount;
+            data.SEFadeTime = nowDialogue.SEFadeTime;
+            data.SEEndFadeTime = nowDialogue.SEEndFadeTime;
+
+            data.BGM = nowDialogue.BGM;
+            data.BGMStyle = nowDialogue.BGMStyle != SoundStyle.UnChange ? nowDialogue.BGMStyle : data.BGMStyle;
+            data.BGMLoop = nowDialogue.BGMLoop;
+            data.BGMCount = nowDialogue.BGMCount;
+            data.BGMFadeTime = nowDialogue.BGMFadeTime;
+            data.BGMEndFadeTime = nowDialogue.BGMEndFadeTime;
+
+            for (int charaIndex = 0; charaIndex < data.charas.Length; charaIndex++)
+            {
+                data.charas[charaIndex] = data.howCharas[charaIndex] != CharaChangeStyle.UnChange ? nowDialogue.charas[charaIndex] : data.charas[charaIndex];
+                data.charaEffects[charaIndex] = data.charaEffects[charaIndex] != Effect.UnChange ? nowDialogue.charaEffects[charaIndex] : data.charaEffects[charaIndex];
+                data.charaEffectStrength[charaIndex] = data.charaEffectStrength[charaIndex];
+            }
+
         }
     }
 
@@ -128,5 +222,12 @@ namespace NovelEditor
         public int paragraphIndex;
         public int dialogueIndex;
         public List<int> passedParagraphId;
+    }
+
+    internal struct SkipedData
+    {
+        public NovelData.ParagraphData.Dialogue dialogue;
+        public int ParagraphIndex;
+        public Next next;
     }
 }
