ac74e2a3889e637830015ca72e6b4bec772837ad
リル姉 <95098112+chocorone@users.noreply.github.com>
2022-11-29 01:28:47 +0900

    [Fix] ミュート機能に修正

--- Assets/NovelEditor/Sample/NovelGame/ValueManager.cs
+++ Assets/NovelEditor/Sample/NovelGame/ValueManager.cs
@@ -12,12 +12,14 @@ namespace NovelEditor.Sample
         [SerializeField] Slider BGMslider;
         [SerializeField] Slider SEslider;
         [SerializeField] Slider Textslider;
+        [SerializeField] Toggle muteToggle;
         // Start is called before the first frame update
         void Start()
         {
             BGMslider.onValueChanged.AddListener((value) => { player.BGMVolume = value; });
             SEslider.onValueChanged.AddListener((value) => { player.SEVolume = value; });
             Textslider.onValueChanged.AddListener((value) => { player.textSpeed = (int)value; });
+            muteToggle.onValueChanged.AddListener((value) => { player.mute = value; });
             player.OnBegin += () => { Debug.Log("begin"); };
             player.OnEnd += () => { Debug.Log("end"); };
             player.OnChoiced += (name) => { Debug.Log(name); };
