1b67ff29dec1b476b09b28631c1fad64665d22fb
リル姉 <95098112+chocorone@users.noreply.github.com>
2022-11-30 01:50:28 +0900

    [Add] バックログのサンプルの追加

--- Assets/NovelEditor/Sample/NovelGame/ValueManager.cs
+++ Assets/NovelEditor/Sample/NovelGame/ValueManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using NovelEditor;
+using TMPro;
 
 namespace NovelEditor.Sample
 {
@@ -13,9 +14,12 @@ namespace NovelEditor.Sample
         [SerializeField] Slider SEslider;
         [SerializeField] Slider Textslider;
         [SerializeField] Toggle muteToggle;
+        [SerializeField] GameObject panel;
+        [SerializeField] TextMeshProUGUI text;
         // Start is called before the first frame update
         void Start()
         {
+            text.text = "";
             BGMslider.onValueChanged.AddListener((value) => { player.BGMVolume = value; });
             SEslider.onValueChanged.AddListener((value) => { player.SEVolume = value; });
             Textslider.onValueChanged.AddListener((value) => { player.textSpeed = (int)value; });
@@ -23,12 +27,29 @@ namespace NovelEditor.Sample
             player.OnBegin += () => { Debug.Log("begin"); };
             player.OnEnd += () => { Debug.Log("end"); };
             player.OnChoiced += (name) => { Debug.Log(name); };
+            player.OnSkiped += () => { text.text = ""; };
+            player.OnLoad += () => { text.text = ""; };
+            player.OnBegin += () => { text.text = ""; };
+            player.OnDialogueChanged += (data) => { text.text += data.Name + "　　<indent=15%> 「" + data.text + "」</indent>\n\n"; };
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (panel.activeSelf)
+                {
+                    player.UnPause();
+                    panel.SetActive(false);
 
+                }
+                else
+                {
+                    player.Pause();
+                    panel.SetActive(true);
+                }
+            }
         }
     }
 
