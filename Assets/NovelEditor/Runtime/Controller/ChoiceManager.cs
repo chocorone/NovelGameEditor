335b13adcccc211ce40745255d38274d5c3af927
リル姉 <95098112+chocorone@users.noreply.github.com>
2022-12-31 10:11:15 +0900

    [Add] コメントの追加

--- Assets/NovelEditor/Runtime/Controller/ChoiceManager.cs
+++ Assets/NovelEditor/Runtime/Controller/ChoiceManager.cs
@@ -7,9 +7,17 @@ using System;
 
 namespace NovelEditor
 {
+    /// <summary>
+    /// 選択肢を管理するクラス
+    /// </summary>
     internal class ChoiceManager : MonoBehaviour
     {
         private ChoiceButton _button;
+
+        /// <summary>
+        /// 初期化用関数
+        /// </summary>
+        /// <param name="button">選択肢で使用するボタン</param>
         internal void Init(ChoiceButton button)
         {
             _button = button;
@@ -19,6 +27,11 @@ namespace NovelEditor
             }
         }
 
+        /// <summary>
+        /// 選択肢を設定し、選択を待つ
+        /// </summary>
+        /// <param name="datas">選択肢のデータ</param>
+        /// <param name="token">使用するCancellationToken</param>
         internal async UniTask<NovelData.ChoiceData> WaitChoice(List<NovelData.ChoiceData> datas, CancellationToken token)
         {
             List<UniTask<NovelData.ChoiceData>> wait = new();
@@ -31,15 +44,18 @@ namespace NovelEditor
             }
 
             var sendData = await UniTask.WhenAny(wait);
+
+            //作成したボタンを全て壊す、オブジェクトプールにしたい
             for (int i = 0; i < transform.childCount; i++)
             {
                 Destroy(transform.GetChild(i).gameObject);
             }
             return sendData.result;
-
-
         }
 
+        /// <summary>
+        /// 選択肢を全て削除する
+        /// </summary>
         internal void ResetChoice()
         {
             for (int i = 0; i < transform.childCount; i++)
